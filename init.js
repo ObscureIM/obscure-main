595191784da2f50eb56b57474cf849bbd9edb3e4
Syahrul <pauscrypto@gmail.com>
2019-02-24 14:12:56 +0800

    Fixed all major bugs

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var app = express();
+var path = require('path');
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -21,3 +22,34 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   })
+
+//get for faq
+app.get('/faq',function(req,res) {
+  res.sendFile(path.join(__dirname +'/website'+ '/faq.html'));
+})
+
+app.get('/wallets',function(req,res) {
+  res.sendFile(path.join(__dirname +'/website'+ '/wallets.html'));
+})
+
+app.get('/news',function(req,res) {
+  res.redirect('https://medium.com')
+
+})
+
+app.get('/notices',function(req,res) {
+  res.redirect('https://medium.com')
+
+})
+
+app.get('/roadmap',function(req,res) {
+  res.sendFile(path.join(__dirname +'/website'+ '/roadmap.html'));
+})
+
+app.get('/privacy-policy',function(req,res) {
+  res.sendFile(path.join(__dirname +'/website'+ '/privacy-policy.html'));
+})
+
+app.get('/terms-of-use',function(req,res) {
+  res.sendFile(path.join(__dirname +'/website'+ '/terms-of-use.html'));
+})
